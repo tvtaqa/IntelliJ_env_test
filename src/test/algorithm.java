f5b248a644d6fa1731584f1b6c4c5702bdf6d28c
tvtaqa <565441220@qq.com>
2020-12-22 22:33:46 +0800

    first revise

--- src/test/algorithm.java
+++ src/test/algorithm.java
@@ -2,6 +2,7 @@ package test;
 
 public class algorithm {
     public static void main(String[] args) {
+        System.out.println("github revise");
         System.out.println("hello world");
     }
 }
